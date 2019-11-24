b587d1bd92a12dbd92ecfb7d0b435dd9b9368e10
chrisna2 <chrisna2@hanmail.net>
2019-11-24 14:22:23 +0900

    first git commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App - hello wordld why I can't connect git"/>
   </div>
 </template>
 
